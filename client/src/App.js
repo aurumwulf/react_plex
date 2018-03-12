df46b6e2595cee52443d26ac5e77d87a598b26cc
Tyson Au <au.tyson@gmail.com>
2018-03-12 17:06:31 -0600

    Add data query from Movies

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,13 @@ const App = () => (
       <NavBar />
     </div>
     <Switch>
-      <div className="ui center aligned container">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/movies" component={Movies} />
+      <div class="stretched twelve wide column">
+        <div class="ui segment">
+          <div className="ui container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/movies" component={Movies} />
+          </div>
+        </div>
       </div>
       <Route component={NoMatch} />
     </Switch>
--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Movies extends React.Component {
   state = { movies: [] };
 
+  componentDidMount() {
+    axios.get('/api/movies').then(res => this.setState({ movies: res.data }));
+  }
+
   render() {
-    return <h1>Movies</h1>;
+    let { movies } = this.state;
+    return (
+      <div>
+        <h1>Movies</h1>
+        <div role="list" className="ui list">
+          {movies.map(m => (
+            <div role="listitem" className="item">
+              <i aria-hidden="true" className="marker icon" />
+              <div key={m.id} className="content">
+                <Link className="header" to={`/movie/${m.id}`}>
+                  {m.title}
+                </Link>
+                <div className="description">Directed by {m.director}</div>
+              </div>
+              <br />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
